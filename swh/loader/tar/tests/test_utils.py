6a50f1dcff24bc3e005978df5a7358c897305947
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2015-10-22 11:52:04 +0200

    Refactor - add tests on name/rel/ext computations

--- swh/loader/tar/tests/test_utils.py
+++ swh/loader/tar/tests/test_utils.py
@@ -11,54 +11,91 @@ from swh.loader.tar import utils
 
 
 class TestUtils(unittest.TestCase):
-    @istest
-    def release_number(self):
-        files = {
-            'free-ipmi-1.2.2.tar': '1.2.2',
-            'free-ipmi-1.2.2.tar.gz': '1.2.2',
-            'free-ipmi-1.2.2.tar.tgz': '1.2.2',
-            'gcc-testsuite-4.4.2-4.4.3.diff.bz2': '4.4.2-4.4.3.diff',
-            'gcc-java-4.0.4.tar.gz': '4.0.4',
-            'gmp-2.0.tar.lzma': '2.0',
-            'win-gerwin-0.6.zip': '0.6',
-            'ballandpaddle-0.8.0.tar.xz': '0.8.0',
-            'mail-1.1.1.some.lz': '1.1.1.some',
-            'gmp-4.1.1-4.1.2.diff.tar.Z': '4.1.1-4.1.2.diff',
-            'findutils-4.2.18.tar.bzip2': '4.2.18',
-            'greg-1.4.tar.gz': '1.4',
+    @classmethod
+    def setupClass(cls):
+        cls.files = {
+            'free-ipmi-1.2.2.tar': ('free-ipmi-', '1.2.2', '.tar'),
+            'free-ipmi-1.2.2.tar.gz': ('free-ipmi-', '1.2.2', '.tar.gz'),
+            'free-ipmi-1.2.2.tar.tgz': ('free-ipmi-', '1.2.2', '.tar.tgz'),
+            'gcc-testsuite-4.4.2-4.4.3.diff.bz2':
+            ('gcc-testsuite-', '4.4.2-4.4.3.diff', '.bz2'),
+            'gcc-java-4.0.4.tar.gz': ('gcc-java-', '4.0.4', '.tar.gz'),
+            'gmp-2.0.tar.lzma': ('gmp-', '2.0', '.tar.lzma'),
+            'win-gerwin-0.6.zip': ('win-gerwin-', '0.6', '.zip'),
+            'ballandpaddle-0.8.0.tar.xz':
+            ('ballandpaddle-', '0.8.0', '.tar.xz'),
+            'mail-1.1.1.some.lz': ('mail-', '1.1.1.some', '.lz'),
+            'gmp-4.1.1-4.1.2.diff.tar.Z':
+            ('gmp-', '4.1.1-4.1.2.diff', '.tar.Z'),
+            'findutils-4.2.18.tar.bzip2':
+            ('findutils-', '4.2.18', '.tar.bzip2'),
 
             # . separator
-            'greg.1.4.tar.gz': '1.4',
+            'greg-1.4.tar.gz': ('greg-', '1.4', '.tar.gz'),
 
             # number in software product
-            'aspell6-pt_BR-20070411-0.tar.bz2': '20070411-0',
-            'libosip2-3.3.0.tar.gz': '3.3.0',
+            'aspell6-pt_BR-20070411-0.tar.bz2':
+            ('aspell6-pt_BR-', '20070411-0', '.tar.bz2'),
+            'libosip2-3.3.0.tar.gz': ('libosip2-', '3.3.0', '.tar.gz'),
 
             # particular patterns...
-            'gift-0.1.9+3epsilon.tar.gz': '0.1.9+3epsilon',
-            'gift-0.1.6pre2.tgz': '0.1.6pre2',
-            'binutils-2.19.1a.tar.bz2': '2.19.1a',
-            'readline-4.2-4.2a.diff.gz': '4.2-4.2a.diff',
+            'gift-0.1.9+3epsilon.tar.gz':
+            ('gift-', '0.1.9+3epsilon', '.tar.gz'),
+            'gift-0.1.6pre2.tgz': ('gift-', '0.1.6pre2', '.tgz'),
+            'binutils-2.19.1a.tar.bz2': ('binutils-', '2.19.1a', '.tar.bz2'),
+            'readline-4.2-4.2a.diff.gz': ('readline-', '4.2-4.2a.diff', '.gz'),
 
             # with arch patterns
-            'cvs-1.12.6-BSD.bin.gz': '1.12.6-BSD.bin',
-            'cvs-1.12.12-SunOS-5.8-i386.gz': '1.12.12-SunOS-5.8-i386',
-            'gnutls-3.0.20-w32.zip': '3.0.20-w32',
+            'cvs-1.12.6-BSD.bin.gz': ('cvs-', '1.12.6-BSD.bin', '.gz'),
+            'cvs-1.12.12-SunOS-5.8-i386.gz':
+            ('cvs-', '1.12.12-SunOS-5.8-i386', '.gz'),
+            'gnutls-3.0.20-w32.zip': ('gnutls-', '3.0.20-w32', '.zip'),
             'mit-scheme_7.7.90+20080130-0gutsy1.diff.gz':
-            '7.7.90+20080130-0gutsy1.diff',
+            ('mit-scheme_', '7.7.90+20080130-0gutsy1.diff', '.gz'),
 
             # no release number
-            'gnu.ps.gz': None,
-            'direvent-latest.tar.gz': None,
+            'gnu.ps.gz': ('gnu', None, '.gz'),
+            'direvent-latest.tar.gz': ('direvent-latest', None, '.tar.gz'),
         }
 
-        # then
-        for f in files.keys():
-            rel_num = utils.release_number(f)
+    @istest
+    def release_number(self):
+        for f in self.files.keys():
+            # when
+            actual_rel_num = utils.release_number(f)
+
+            # then
+            _, expected_rel_num, _ = self.files[f]
             self.assertEquals(
-                files[f],
-                rel_num,
-                'for %s, the version should be %s' % (f, files[f]))
+                actual_rel_num,
+                expected_rel_num,
+                'for %s, the version should be %s' % (f, expected_rel_num))
+
+    @istest
+    def extension(self):
+        for f in self.files.keys():
+            # when
+            actual_ext = utils._extension(f)
+
+            # then
+            _, _, expected_ext = self.files[f]
+            self.assertEquals(
+                actual_ext,
+                expected_ext,
+                'for %s, the version should be %s' % (f, expected_ext))
+
+    @istest
+    def software_name(self):
+        for f in self.files.keys():
+            # when
+            actual_name = utils._software_name(f)
+
+            # then
+            expected_name, _, _ = self.files[f]
+            self.assertEquals(
+                actual_name,
+                expected_name,
+                'for %s, the version should be %s' % (f, expected_name))
 
     @istest
     def commonname(self):
